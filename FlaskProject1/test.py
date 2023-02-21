600d8f1d24aec75c13b04ad353b6aae31367e666
Lordmozz <moziahmichel57@gmail.com>
2023-02-20 20:31:45 -0500

    Messed with message
    ooggaa boogaa

--- FlaskProject1/test.py
+++ FlaskProject1/test.py
@@ -1,13 +1,40 @@
-from flask import Flask, render_template
+import sqlite3
 
+from flask import Flask, render_template, request, url_for, flash, redirect
+import sqlite3
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] ='3c6d13f239288f6857108c4fb2aeb9af98df0e08913795b0'
 messages = [{'title': 'Message One',
              'content': 'Message One Content'},
             {'title': 'Message Two',
              'content': 'Message Two Content'}
             ]
+connection = sqlite3.connect("messages.db",check_same_thread=False)
+cursor = connection.cursor()
+cursor.execute("Create Table IF NOT EXISTS messages (title TEXT, content TEXT)")
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', messages=messages)
+    messagesDB = cursor.execute("SELECT title, content from messages").fetchall()
+    print(messagesDB)
+    return render_template('index.html', messages=messagesDB)
+
+
+@app.route('/create/', methods=('GET','POST'))
+def create ():
+    if request.method == 'POST':
+        title = request.form ['title']
+        content = request.form ['content']
+
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('content is required')
+        else:
+            messages.append({'title': title, 'content': content})
+            cursor.execute("INSERT INTO messages VALUES (?,?)", (title, content))
+            return redirect(url_for('index'))
+
+    return render_template('create.html')
+
